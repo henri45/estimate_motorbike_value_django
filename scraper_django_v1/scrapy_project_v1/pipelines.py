dabeb264a9a921fdb3fcb7dcb1394f8cf7045c33
Henri Terrasse <henri.11@hotmail.fr>
2020-04-13 15:30:20 +0200

    -a

--- scraper_django_v1/scrapy_project_v1/pipelines.py
+++ scraper_django_v1/scrapy_project_v1/pipelines.py
@@ -8,5 +8,29 @@
 
 class ScrapyProjectV1Pipeline(object):
     def process_item(self, item, spider):
-      item.save()
-        return item
+      dynamodb = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url="https://dynamodb.us-east-2.amazonaws.com")
+      try:
+        table = dynamodb.Table('ads')
+      except
+        print("Table doesn't exist")
+
+      response = table.put_item(
+         Item={
+              'url' : item.url
+              'title': item.title
+              'price': item.price
+              'creation_date': item.creation_date
+              'views': item.views
+              'location': item.location
+              'description': item.description
+              'seller_type': item.seller_type
+              'make': item.make
+              'model': item.model
+              'engine_displacement': item.engine_displacement
+              'year': item.year
+              'kilometers': item.kilometers
+              'date_scraping': item.date_scraping
+          }
+      )
+
+      print(item.title, " in the DB")
